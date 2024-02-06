f6b46450cd512557906b2a0be174ec286c49fcdb
almario12345 <almarioelopre1@gmail.com>
2024-02-06 09:37:30 +0800

    second

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const cors = require('cors');
 
 const app = express();
 
+app.use("/assets",express.static("assets"));
+
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req, res) => {
+// app.get('/',(req, res) => {
 
-    res.json({message: 'Restful API BACKEND Using Express'});
+//     res.json({message: 'Restful API BACKEND Using Express'});
 
-});
+// });
+
+app.get('/', (req, res) => {
+
+    //res.json({ message: 'Restfull API Backend Using ExpresJS' });
+    res.sendFile(__dirname + "/page.html");
+    
+    });
+    
 
 app.listen(PORT, () => {
     console.log(`Server is Running on http://localhost:${PORT}`);
