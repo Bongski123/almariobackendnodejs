26784afe4bc2c09ba925cb3450ebc897ac001242
almario12345 <almarioelopre1@gmail.com>
2024-02-19 00:25:14 +0800

    csewe

--- app/routes/publicationRoutes.js
+++ app/routes/publicationRoutes.js
@@ -27,7 +27,7 @@ router.post('/PubRegister', async (req, res) =>{
 
         try {
     
-            db.query('SELECT publication_id, title ,abstract,publication_date,publication_type,publication_year, user_id,project_id FROM publication', (err , result)=> {
+            db.query('SELECT publication_id, title ,abstract,publication_date,publication_type,publication_year, id,project_id FROM publication', (err , result)=> {
                 
                 if(err){
                     console.error('Error fetching items:', err);
@@ -52,7 +52,7 @@ router.post('/PubRegister', async (req, res) =>{
     
         try{
     
-            db.query('SELECT publication_id, title, authors,abstract,publication_date ,publication_type,publication_year, user_id,project_id FROM publication WHERE publication_id = ?', publication_id, (err, result)=>{
+            db.query('SELECT publication_id, title, authors,abstract,publication_date ,publication_type,publication_year, id,project_id FROM publication WHERE publication_id = ?', publication_id, (err, result)=>{
     
                 if(err){
                     console.error('Error fetcing items:', err);
@@ -73,14 +73,14 @@ router.put('/updatepub/:id', authenticateToken, async(req, res)=>{
 
     let project_id =req.params.id;
 
-    const {title, authors,abstract,publication_type,publication_year, user_id} = req.body;
+    const {title, authors,abstract,publication_type,publication_year, id} = req.body;
 
-    if(!project_id || !title || !authors || !abstract || publication_type ||!publication_year|| user_id ){
+    if(!project_id || !title || !authors || !abstract || publication_type ||!publication_year|| id ){
         return res.status(400).send({ error: role , message: 'Please provide  role_code, role_name'});
     }
 
     try{
-        db.query('UPDATE publication SET title = ? , authors =? , abstract =?, publication_type = ? ,publication_year =?,user_id =?  WHERE publication_id =?', [title ,authors,abstract,publication_type,user_id, project_id],(err, result, field) =>{
+        db.query('UPDATE publication SET title = ? , authors =? , abstract =?, publication_type = ? ,publication_year =?,id =?  WHERE publication_id =?', [title ,authors,abstract,publication_type,user_id, project_id],(err, result, field) =>{
 
           if(err){
             console.error('Error updating items:', err);
