f39b9af07e79516c0ed5c4c9d0b396d2d07c1d97
almario12345 <almarioelopre1@gmail.com>
2024-02-21 00:21:59 +0800

    ewrgr

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express =require('express');
 const cors = require('cors');
-const upload =require('express-upload');
+
 const multer = require('multer') //http://expressjs.com/en/resources/middleware/multer.html npm install --save multer
 const app = express();
 
 app.use("/assets",express.static("assets"));
 
 app.use(cors());
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 9000;
 const bodyParser = require('body-parser');
 const UsersRoutes = require('./app/routes/user');
 const RolesRoutes = require('./app/routes/roleRoutes');
@@ -18,7 +18,7 @@ const departmentRoutes = require('./app/routes/departmentRountes');
 const testAPIRouter = require('./app/routes/testAPI');
 const attachementRoutes = require('./app/routes/attachmentsRoutes');
 const projectRoutes = require('./app/routes/ProjectRoutes');
-const uploadRoutes = require('./app/routes/uploadroutes');
+
 
 app.use("/assets",express.static("assets"));
 
@@ -39,7 +39,8 @@ app.use('/api', projectRoutes);
 app.use("/testAPI",testAPIRouter);
 app.use("/api",departmentRoutes);
 app.use("/api",attachementRoutes);
-app.use("/api",uploadRoutes);
+
+
 
 
 const storage = multer.diskStorage({
@@ -53,7 +54,23 @@ const storage = multer.diskStorage({
    
   const upload = multer({storage})
    
- 
+  app.post('/upload', upload.single('file'), (req, res) => {
+   console.log(req.body)
+   console.log(req.file)
+   return res.json({Status: "Success"});
+  })
+   
+  app.post('/create',upload.single('file'), (req, res) => {
+      const sql = "INSERT INTO attachments (`file_name`,`docs`) VALUES (?)"; 
+      const values = [
+          req.body.file_name,
+          req.file.filename
+      ]
+      db.query(sql, [values], (err, result) => {
+          if(err) return res.json({Error: "Error singup query"});
+          return res.json({Status: "Success"});
+      })
+  })
  
 
 app.get('/', (req, res) => {
